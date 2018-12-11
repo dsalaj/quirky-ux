96104ed950920913f83317d9377f56559c2aefb5
Darjan Salaj <salaj@gmx.at>
2018-12-11 09:50:39 +0100

    Removed jquery dependency

--- components/flash.js
+++ components/flash.js
@@ -1,19 +1,27 @@
-function flash(){
-  $('.flash')
-   .show()  //show the hidden div
-   .fadeOut(Math.random() * 200 + 100)
-   .css({'opacity': 1});
+function flash(min_time, var_time, flash_overlay_class){
+  var flash_els = document.getElementsByClassName(flash_overlay_class);
+  Array.prototype.forEach.call(flash_els, function(el) {
+    el.style.display = "";
+    var s = el.style;
+    s.opacity = 1;
+    (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,Math.random() * var_time + min_time)})();
+  });
 }
 
-$(document).ready(function() {
-  $('.flash').hide();
-  (function loop() {
-    var mindelay = 200;
-    var maxdelay = 2000;
+function init_flash(flash_overlay_class, mindelay, maxdelay, min_time, var_time) {
+  var flash_els = document.getElementsByClassName(flash_overlay_class);
+  Array.prototype.forEach.call(flash_els, function(el) {
+    el.style.display = "none";
+  });
+  function loop() {
     var rand = Math.round(Math.random() * (maxdelay - mindelay)) + mindelay;
     setTimeout(function() {
-            flash();
-            loop();
+      flash(min_time, var_time, flash_overlay_class);
+      loop();
     }, rand);
-}());
-});
+  };
+  loop();
+};
+
+init_flash("flash", 500, 2000, 10, 30);
+init_flash("flash-horror", 50, 500, 4, 8);
